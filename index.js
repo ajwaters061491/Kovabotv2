32bab3e4aaf0461bf1f39934169394c94873bced
Alvin Waters <alvin.waters0614@gmail.com>
2018-12-27 22:53:02 -0500

    added commands

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const tmi = require("tmi.js");
-const info = require("./botinfo.json");
+const info = require('./botinfo.json');
 //Kovabotv2
 
 
@@ -22,5 +22,24 @@ const options = {
 
 const client = new tmi.client(options);
 
-// Connect the client to the server..
+//Connect the client to the server..
 client.connect();
+
+client.on("chat", function (channel, userstate, message, self) {
+    //ignore chat from the bot itself
+    if (self) return;
+
+    //commands go here
+    if (message.toLowerCase() === '!hi' || message.toLowerCase() === '!hello') {
+        client.action(channel, `Hello ${userstate.username}, and welcome to the stream!`);
+    } 
+    else if (message.toLowerCase() === '!twitter') {
+        client.action(channel, `${userstate.username} my twitter is @Kovapls! Follow for shitposts and unfunny content!`);
+    }
+    else if (message.toLowerCase() === '!github') {
+        client.action(channel, `${userstate.username} my github is ajwaters061491`);
+    }
+    else if (message.toLowerCase() === '!ree') {
+        client.action(channel, 'REEEEEEEEEEEEEEE');
+    }
+});
