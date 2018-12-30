b8cb63fa2fa72a1a332460506cc6c7c4470cbcd5
Alvin Waters <alvin.waters0614@gmail.com>
2018-12-30 02:04:31 -0500

    added package file, and attempted to prevent spam

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const tmi = require("tmi.js");
 const info = require('./botinfo.json');
 const commandParse = require('./commands.js')
+var chatLimiter = true;
+
 //Kovabotv2
 
 
@@ -26,10 +28,10 @@ const client = new tmi.client(options);
 //Connect the client to the server..
 client.connect();
 
-client.on("chat", function (channel, userstate, message, self) {
+client.on("chat", function (channel, userstate, message, self) {   //TODO wrap in a timer of around 5 seconds to prevent spamming, add message for on close.
     //ignore chat from the bot itself
     if (self) return;
-
+  
     commandParse.read(channel, userstate, message, client); //calls the function to handle chat parsing
-
 });
+
